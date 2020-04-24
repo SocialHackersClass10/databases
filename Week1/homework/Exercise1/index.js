cba7964ad6b671f1c84f1cfccc4dbcd6b0d97e92
Abdullah SEN <sen-abdullah@yandex.com>
2020-04-25 00:34:47 +0300

    connects mysql server

--- Week1/homework/Exercise1/index.js
+++ Week1/homework/Exercise1/index.js
@@ -1,4 +1,14 @@
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'hyfuser',
+    password: 'hyfpassword'
 
+})
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
